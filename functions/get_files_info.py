cc3dbb0a44541f8954b1cc2b70ce966086d7fe77
mastergunk <markusdanp@gmail.com>
2025-06-29 21:39:22 +0200

    Get files info function is functional

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,16 +3,35 @@ import os
 
 def get_files_info(working_directory, directory=None):
 
-    if directory not in working_directory:
-        return f"Error: Cannot list '{directory}' as it is outside the permitted working directory"
-    
-    if directory != dict:
-        return f"Error: '{directory}' is not a directory"
+    abs_path_working_dir = os.path.abspath(working_directory)
+    joined_paths = os.path.join(abs_path_working_dir, directory)
+    joined_paths_abs = os.path.abspath(joined_paths)
+
+    if joined_paths_abs.startswith(abs_path_working_dir) == False:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(joined_paths_abs):
+        return f'Error: "{directory}" is not a directory'
     
     else:
         try:
-            return f"{directory}: file_size={os.path.getsize(directory)} bytes, is_dir={os.path.isdir(directory)}"
-        except OSError as e:
-            print(f"Error: {e}")
+            result_list = []
+
+            for item in os.listdir(joined_paths_abs):
+
+                full_path_to_check = os.path.join(joined_paths_abs, item)
+                file_size = 0
+
+                check_dir = os.path.isdir(full_path_to_check)
+                file_size = os.path.getsize(full_path_to_check)
+
+                result_list.append(f"{item}: file_size={file_size}, is_dir={check_dir}")
+                
+
+            return  "\n".join(result_list)
+        
+        except Exception as e:
+            return f"Error: {e}"
+    
 
     
